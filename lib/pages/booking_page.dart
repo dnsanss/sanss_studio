0387ecb93f314b753b0aa2bdf7278cc1ae5bc68a
Dianmargi <dionms203@gmail.com>
2025-05-24 20:58:58 +0700

    menambahkan halaman booking sukses

--- lib/pages/booking_page.dart
+++ lib/pages/booking_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sanss_studio/pages/booking_sukses_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class BookingPage extends StatefulWidget {
@@ -11,7 +12,7 @@ class BookingPage extends StatefulWidget {
 }
 
 class _BookingPageState extends State<BookingPage> {
-  int jumlahOrang = 1;
+  int jumlahTiket = 1;
   late int harga;
   late int totalHarga;
   String? userName;
@@ -46,22 +47,63 @@ class _BookingPageState extends State<BookingPage> {
 
   void updateTotalHarga() {
     setState(() {
-      totalHarga = harga * jumlahOrang;
+      totalHarga = harga * jumlahTiket;
     });
   }
 
   Future<void> konfirmasiBooking() async {
-    final userId = Supabase.instance.client.auth.currentUser?.id;
-    await Supabase.instance.client.from('booking').insert({
-      'users': userId,
-      'schedules': widget.film['id'],
-      'jumlah_orang': jumlahOrang,
-      'price': totalHarga,
-    });
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(const SnackBar(content: Text('Tiket berhasil dipesan!')));
-    Navigator.pop(context);
+    final confirm = await showDialog<bool>(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: const Text('Konfirmasi'),
+            content: const Text('Apakah Anda yakin ingin memesan tiket ini?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, false),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, true),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+    );
+
+    if (confirm == true) {
+      final userId = Supabase.instance.client.auth.currentUser?.id;
+
+      // Menyimpan data ke Supabase dan mendapatkan record yang baru saja dibuat
+      // Insert and get the new booking id
+      final insertResponse =
+          await Supabase.instance.client
+              .from('booking')
+              .insert({
+                'user_id': userId,
+                'schedules_id': widget.film['id'],
+                'jumlah_tiket': jumlahTiket,
+                'price': totalHarga,
+              })
+              .select('id')
+              .single();
+
+      final bookingId = insertResponse['id'];
+
+      final bookingResponse =
+          await Supabase.instance.client
+              .from('booking')
+              .select('*, schedules: schedules_id(*), user_id: users(*)')
+              .eq('id', bookingId)
+              .maybeSingle();
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => BookingSuksesPage(booking: bookingResponse ?? {}),
+        ),
+      );
+    }
   }
 
   @override
@@ -77,12 +119,23 @@ class _BookingPageState extends State<BookingPage> {
             Center(
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                child: Image.network(
-                  film['image_url'],
-                  width: 160,
-                  height: 220,
-                  fit: BoxFit.cover,
-                ),
+                child:
+                    film['image_url'] != null
+                        ? Image.network(
+                          film['image_url'],
+                          width: 160,
+                          height: 220,
+                          fit: BoxFit.cover,
+                        )
+                        : Container(
+                          width: 160,
+                          height: 220,
+                          color: Colors.grey[300],
+                          child: const Icon(
+                            Icons.image_not_supported,
+                            size: 48,
+                          ),
+                        ),
               ),
             ),
             const SizedBox(height: 20),
@@ -134,25 +187,25 @@ class _BookingPageState extends State<BookingPage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Text(
-                  'Berapa Orang',
+                  'Jumlah Tiket',
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
                 Row(
                   children: [
                     IconButton(
                       onPressed:
-                          jumlahOrang > 1
+                          jumlahTiket > 1
                               ? () {
-                                setState(() => jumlahOrang--);
+                                setState(() => jumlahTiket--);
                                 updateTotalHarga();
                               }
                               : null,
                       icon: const Icon(Icons.remove_circle_outline),
                     ),
-                    Text('$jumlahOrang', style: const TextStyle(fontSize: 18)),
+                    Text('$jumlahTiket', style: const TextStyle(fontSize: 18)),
                     IconButton(
                       onPressed: () {
-                        setState(() => jumlahOrang++);
+                        setState(() => jumlahTiket++);
                         updateTotalHarga();
                       },
                       icon: const Icon(Icons.add_circle_outline),
---
+++ lib/pages/booking_sukses_page.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:sanss_studio/pages/home_pages.dart';
+
+class BookingSuksesPage extends StatefulWidget {
+  final Map<String, dynamic> booking;
+
+  const BookingSuksesPage({super.key, required this.booking});
+
+  @override
+  State<BookingSuksesPage> createState() => _BookingSuksesPageState();
+}
+
+class _BookingSuksesPageState extends State<BookingSuksesPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showSuccessDialog(context);
+    });
+  }
+
+  void showSuccessDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder:
+          (context) => AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.blue[50],
+                  radius: 36,
+                  child: Icon(Icons.check_circle, color: Colors.blue, size: 60),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  "Terima kasih telah memesan tiket di aplikasi kami. Silakan datang tepat waktu dan nikmati filmnya!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            actions: [
+              Align(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text("OK", style: TextStyle(fontSize: 16)),
+                ),
+              ),
+            ],
+          ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final schedules = widget.booking['schedules']; // relasi ke tabel movies
+    final users = widget.booking['user_id']; // relasi ke tabel users
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Booking Telah Berhasil')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child:
+                  schedules['image_url'] != null
+                      ? Image.network(
+                        schedules['image_url'],
+                        width: 160,
+                        height: 220,
+                        fit: BoxFit.cover,
+                      )
+                      : Container(
+                        width: 160,
+                        height: 220,
+                        color: Colors.grey[300],
+                        child: const Icon(Icons.image_not_supported, size: 48),
+                      ),
+            ),
+
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Nama Pemesan :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "${users?['nama'] ?? '-'}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "ID Pemesanan :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "${widget.booking['id']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Judul Film :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "${schedules['tittle']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Jadwal :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "${schedules['day']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Jam Tayang :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "${schedules['time']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Jumlah Tiket :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "${widget.booking['jumlah_tiket']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Total Harga :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Rp ${widget.booking['price']}",
+                  style: const TextStyle(fontSize: 16, color: Colors.green),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                backgroundColor: Colors.blueAccent,
+              ),
+              child: const Text(
+                "Kembali ke Halaman Utama",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed:
+                  () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
